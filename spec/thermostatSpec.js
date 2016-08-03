f0a730a1e996e56e16f81e6c391296951a30a5a2
Robert Summers <rsummers02@gmail.com>
2016-08-03 15:10:29 +0100

    user stories 3 and 4

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,14 +9,38 @@ beforeEach(function(){
 });
 
   it('starts at a temperature of 20 degrees', function(){
-    expect(thermostat._DefaultTemperature).toEqual(20);
+    expect(thermostat._temperature).toEqual(20);
   });
 
   describe('increase temperature', function(){
     it('increases temperature by using the up button', function(){
       thermostat.up();
-      expect(thermostat._DefaultTemperature).toBeGreaterThan(20);
+      expect(thermostat.currentTemperature()).toEqual(21);
     });
+  });
+
+  describe('decrease temperature', function() {
+
+    it('decreases temperatur by using the down button', function() {
+      thermostat.down();
+      expect(thermostat.currentTemperature()).toEqual(19);
+    });
+
+    it('has a minimum temperature of 10 degrees', function() {
+      thermostat.down();
+      thermostat.down();
+      thermostat.down();
+      thermostat.down();
+      thermostat.down();
+      thermostat.down();
+      thermostat.down();
+      thermostat.down();
+      thermostat.down();
+      thermostat.down();
+      expect(function() {thermostat.down();}).toThrowError("The minimum temperature is 10");
+    });
+
+
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,9 +1,21 @@
 'user strict';
 
 function Thermostat (){
-  this._DefaultTemperature = 20;
+  this._temperature = 20;
+  this._minimumTemp = 10;
 }
 
 Thermostat.prototype.up = function (){
-  this._DefaultTemperature += 1;
+  this._temperature += 1;
+};
+
+Thermostat.prototype.down = function () {
+  if (this._minimumTemp >= this.currentTemperature()) {
+    throw new Error("The minimum temperature is 10");
+  }
+  this._temperature -= 1;
+};
+
+Thermostat.prototype.currentTemperature = function () {
+  return this._temperature;
 };
