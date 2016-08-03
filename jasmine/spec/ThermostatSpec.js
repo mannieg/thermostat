d3390de112ec44acd78f9fa1b819ea6a069a645d
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-03 13:35:47 +0100

    Implemented thermostat logic

--- jasmine/spec/ThermostatSpec.js
+++ jasmine/spec/ThermostatSpec.js
@@ -8,21 +8,55 @@ describe("Thermostat", function() {
   });
 
   it("should start at 20°c", function() {
-    expect(thermostat.temperature()).toEqual(20);
+    expect(thermostat.temperature()).toEqual(thermostat.DEFAULT);
   });
+
   it("increases temperature with up button", function(){
     thermostat.increase();
-    expect(thermostat.temperature()).toBeGreaterThan(20);
+    expect(thermostat.temperature()).toBeGreaterThan(thermostat.DEFAULT);
   });
+
   it("decreases temperature with down button", function(){
     thermostat.decrease();
-    expect(thermostat.temperature()).toBeLessThan(20);
+    expect(thermostat.temperature()).toBeLessThan(thermostat.DEFAULT);
   });
+
   it("does not decrease if temperature is < 10", function(){
-    for (var i = 0; i <= 10; i++) {
+    for (var i = 1; i <= thermostat._minTemp; i++) {
       thermostat.decrease();
     };
-    expect(thermostat.decrease()).toThrow("Cannot go below minimum temperature of 10");
-    // expect(thermostat.temperature()).toEqual(10);
+    expect(thermostat.temperature()).toEqual(thermostat._minTemp);
+  });
+
+  it("resets the temperature back to default", function() {
+    thermostat.increase();
+    thermostat.reset();
+    expect(thermostat.temperature()).toEqual(thermostat.DEFAULT);
+  })
+
+  describe("Power Saving Mode", function() {
+    it("Power saving mode is on by default", function(){
+      expect(thermostat._powerSavingMode).toBeTruthy();
+    });
+
+    it("Set power saving mode to off", function() {
+      thermostat.powerSavingMode(false);
+      expect(thermostat._powerSavingMode).toBeFalsy();
+    });
+
+    it("Max default temperature is 25", function() {
+      for (var i = 1; i <= 5; i++){
+        thermostat.increase();
+      }
+      expect(thermostat.temperature()).toEqual(thermostat._maxTemp);
+    });
+
+    it("Max temperature when off", function() {
+      thermostat.powerSavingMode(false);
+      for(var i = 1; i <= 12; i++){
+        thermostat.increase();
+      }
+      expect(thermostat.temperature()).toEqual(thermostat._maxTemp);
+    });
   });
 });
--- jasmine/src/Thermostat.js
+++ jasmine/src/Thermostat.js
@@ -1,17 +1,41 @@
+'use strict';
+
 function Thermostat() {
-  this._degrees = 20;
+  this.DEFAULT = 20
+  this._maxTemp = 25;
+  this._minTemp = 10;
+  this.currentTemp = this.DEFAULT;
+  this._INCREMENT = 1;
+  this._powerSavingMode = true;
 }
 
 Thermostat.prototype.temperature = function() {
-  return this._degrees;
+  return this.currentTemp;
 };
 
 Thermostat.prototype.increase = function() {
-  this._degrees += 1
+  if (this._powerSavingMode && this.currentTemp <= this._maxTemp)
+    this.currentTemp += this._INCREMENT;
+  else if (this.currentTemp <= this._maxTemp) {
+    this.currentTemp += this._INCREMENT;
+  }
 };
 
 Thermostat.prototype.decrease = function() {
-  if (this._degrees <= 10)
-    {throw "Cannot go below minimum temperature of 10"}
-  else {this._degrees -= 1};
+  if (this.currentTemp >= this._minTemp)
+    this.currentTemp -= this._INCREMENT;
 };
+
+Thermostat.prototype.powerSavingMode = function(mode) {
+  this._powerSavingMode = mode;
+  if (this._powerSavingMode) {
+    this._maxTemp = 25;
+  }
+  else {
+    this._maxTemp = 32;
+  }
+}
+
+Thermostat.prototype.reset = function() {
+  this.currentTemp = this.DEFAULT;
+}
