fde9cb6622b054638584937837aaa5ddeb0a0685
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-03 19:14:30 +0100

    Refactored code, added some more functions

--- src/test.js
+++ src/test.js
@@ -1,7 +1,24 @@
 var thermostat = new Thermostat();
+var currentTemp;
+
+function getTempDecimal(){
+  currentTemp = thermostat.temperature() / thermostat.MAX_TEMP;
+}
 
 $('#increase_btn').click(function() {
   thermostat.increase();
-  var currentTemp = thermostat.temperature() / thermostat.MAX_TEMP;
+  getTempDecimal();
+  bar.animate(currentTemp);
+});
+
+$('#reset_btn').click(function() {
+  thermostat.reset();
+  getTempDecimal();
+  bar.animate(currentTemp);
+});
+
+$('#decrease_btn').click(function() {
+  thermostat.decrease();
+  getTempDecimal();
   bar.animate(currentTemp);
 });
