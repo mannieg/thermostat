9a22e0c94e1e64a442ca85744df6fd3102196deb
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-04 14:51:33 +0100

    Added api to get current weather for input city

--- src/interface.js
+++ src/interface.js
@@ -2,15 +2,16 @@ $ (document).ready(function() {
 
   var thermostat = new Thermostat();
 
+  $('.get-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    get_city_temp(city);
+  });
+
   $( "#temperature-up" ).click(function() {
     thermostat.up();
     update_temp();
   });
-
-  function update_temp(){
-    $('#temperature').text(thermostat.currentTemperature());
-    $('#temperature').attr('class', thermostat.energyUsage());
-  };
 
   $( "#temperature-down" ).click(function() {
     thermostat.down();
@@ -32,5 +33,18 @@ $ (document).ready(function() {
     $('#power-saving-status').text('off');
   });
 
+  function get_city_temp(city) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city;
+    var key = "&APPID=8e294e4884174ccaf295c1098548b598";
+    var units = "&units=metric";
+    $.get(url + key + units, function(data) {
+      $('#outside-temp').text('The temperature for ' + city + ' ' + data.main.temp);
+    });
+  }
+
+  function update_temp(){
+    $('#temperature').text(thermostat.currentTemperature());
+    $('#temperature').attr('class', thermostat.energyUsage());
+  };
 
 });
